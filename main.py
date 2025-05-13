112af05aac2021c5fea7904df59060d949c19ef5
Tobias B. <tobias@tsbonn.de>
2025-05-13 20:24:02 +0200

    Removed unused import, added file upload and more configuration options in config.ini

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import configparser
 import os
 import logging
-import requests
 from tonie_api.api import TonieAPI
 
 config = configparser.ConfigParser()
@@ -11,25 +10,30 @@ config.read(config_file_path)
 try:
     username = config['api_credentials']['username']
     password = config['api_credentials']['password']
+    creative_tonie_name = config['tonie_config']['creative_tonie_name']
+    clear_all_before_upload = (config['app_config']['clear_all_before_upload'] == 'True')
 except (KeyError, configparser.NoSectionError, configparser.NoOptionError) as e:
     print(f"Fehler beim Lesen der Konfigurationsdatei: {e}")
 
-# set up detailed logging
+# Set up detailed logging
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 
+# Connect to the Tonie API and get the list of Creative Tonies
 api=TonieAPI(username, password)
 list_of_tonies = api.get_all_creative_tonies()
 
-# Find the CreativeTonie with the name 'Kreativ-Tonie Johanna Skater'
-tonie_name = "Kreativ-Tonie Johanna Skater"
-selected_tonie = next((tonie for tonie in list_of_tonies if tonie.name == tonie_name), None)
+# Find the CreativeTonie with the name in variable creative_tonie_name
+selected_tonie = next((tonie for tonie in list_of_tonies if tonie.name == creative_tonie_name), None)
 
 if selected_tonie:
-    # Call the clear_all_chapter_of_tonie function with the selected CreativeTonie
-    api.clear_all_chapter_of_tonie(selected_tonie)
-    print(f"Cleared all chapters of the Tonie: {selected_tonie.name}")
+    print(f"Found CreativeTonie: {selected_tonie.name}")
+    if clear_all_before_upload:
+        api.clear_all_chapter_of_tonie(selected_tonie)
+        print(f"Cleared all chapters of the Tonie: {selected_tonie.name}")
+    api.upload_file_to_tonie(selected_tonie, "test.mp3", "Test Chapter")
+    print(f"Uploaded file to Tonie: {selected_tonie.name}")
 else:
-    print(f"No CreativeTonie found with the name '{tonie_name}'")
+    print(f"No CreativeTonie found with the name '{selected_tonie.name}'")
 
 
